1e9ec4d5e7aade81dfe0fc21ae98d6d8de64466e
Kristjan Fale <kristjan.fale@gmail.com>
2020-03-30 01:44:34 +0200

    Animation With JS Trigger (bg.jpg was too big, animation didn't work)

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const text = document.getElementById('text');
+const container = document.getElementById('container');
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = 'Breathe In!';
+  container.classList = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold!';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.classList = 'container shrink';
+    }, holdTime);
+  }, breathTime);
+}
+
+setInterval(breathAnimation, totalTime);
